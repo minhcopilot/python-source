af439c7798e611b2890a676dcfd658a22140896c
minh phan <21115053120333@sv.ute.udn.vn>
2024-05-03 16:53:06 +0700

    up

---
+++ ThuocLongChau_API_Project/mysql_api/main.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI,status,Request
+import mysql.connector
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from queries import INSERT_PRODUCT_QUERY, INSERT_PRICE_QUERY, INSERT_CATEGORY_QUERY
+
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
+
+# Environment variables
+DATABASE_NAME = os.getenv("DB_NAME")
+HOST = os.getenv("DB_HOST")
+PORT = os.getenv("DB_PORT")
+USER = os.getenv("DB_USER")
+PASSWORD = os.getenv("DB_PASSWORD")
+
+db = mysql.connector.connect(
+    host=HOST,
+    user=USER,
+    password=PASSWORD,
+    database=DATABASE_NAME,
+    port=PORT
+)
+cursor = db.cursor()
+
+app = FastAPI()
+
+def save_product(product):
+    product_values = (
+        product["sku"], product["name"], product["webName"], product["image"], product["slug"],
+        product["ingredients"], product["dosageForm"], product["brand"], product["displayCode"],
+        product["isActive"], product["isPublish"], product["searchScoring"], product["productRanking"],
+        product["specification"]
+    )
+    cursor.execute(INSERT_PRODUCT_QUERY, product_values)
+
+    for price in product["prices"]:
+        save_price(product["sku"], price)
+
+    for category in product["category"]:
+        save_category(product["sku"], category)
+
+def save_price(product_sku, price):
+    price_values = (
+        price["id"], product_sku, price["measureUnitCode"], price["measureUnitName"],
+        price["isSellDefault"], price["price"], price["currencySymbol"], price["isDefault"],
+        price["inventory"], price["isInventory"], price["level"]
+    )
+    cursor.execute(INSERT_PRICE_QUERY, price_values)
+
+def save_category(product_sku, category):
+    category_values = (
+        category["id"], category["name"], category["parentName"], category["slug"], category["level"],
+        category["isActive"], product_sku
+    )
+    cursor.execute(INSERT_CATEGORY_QUERY, category_values)
+
+@app.post("/save_products")
+async def save_products(request: Request):
+    try:
+        body = await request.body()
+        if not body:
+            return {"error": "No data received"}, status.HTTP_400_BAD_REQUEST
+
+        data = await request.json()
+        products = data.get("products", [])
+
+        if not products:
+            return {"error": "No products found in the data"}, status.HTTP_400_BAD_REQUEST
+
+        for product in products:
+            save_product(product)
+        db.commit()
+        return {"message": "Data saved successfully"}
+    except ValueError as e:
+        return {"error": str(e)}, status.HTTP_400_BAD_REQUEST
+    except mysql.connector.Error as error:
+        db.rollback()
+        return {"error": str(error)}, status.HTTP_500_INTERNAL_SERVER_ERROR
